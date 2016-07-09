b8341704a8333353cd524bcf80e4144ad44e2a57
Jeremy Smith <jmsmith57@hotmail.com>
2016-07-09 21:04:42 +0100

    getting there ever so slowly

--- main.js
+++ main.js
@@ -7,55 +7,69 @@ $(function(){
   var $gameStart = $("#gameStart");
   var $characterModel = $("#characterModel");
   var $currentScore = $("#score");
-
+  var score = 0;
+  //things still to do: sort out the win condition and make something happen when a player gets a letter wrong, maybe reset the sentence... If they get it right I need a mesage for them to press enter afterwards or perhaps just move on to the next word... Winner and lose conditions with a game reset at the end....Do I do this at the beginning or after do I write a function for each do I change to OOP...too many questions not enough time
+  
+  $currentScore.hide();
   $characterModel.hide();
-  $gameStart.on('click', function(){
-    $(this).fadeOut('slow');
-    $userInput.focus();
-    setTimeout(function(){
-      initEventHandlers();
-      $characterModel.show();
-    },2000);
-  });
-
+  gameIntro();
+  
+  function gameIntro(){
+    $gameStart.on('click', function(){
+      $(this).fadeOut('slow');
+      $userInput.focus();
+      setTimeout(function(){
+        initEventHandlers();
+        $characterModel.fadeIn();
+        $currentScore.show();
+      },2000);
+    });
+  }
   
   function initEventHandlers(){
     $typeThis.prepend(keysToType);
     $userInput.keydown(function() {
-      
+
       if(event.key === keysToType[0]) {
+        $('#typeThis').css("color", "white");
         keysToType.shift();
         $typeThis.text(keysToType.join(""));
-        // setTimeout(function(){
-        //   $typeThis.append("PRESS ENTER");
-        // }, 3000);
-      }else if (event.key!== keysToType[0]){
-        
+      
+      }else if (event.key !== keysToType){
+        $('#typeThis').css("color", "red");
+        console.log("wrong letter");
+        console.log(keysToType);
+      }else{
+        console.log("press enter for next sentence")
       };
     });
 
     $userInputForm.on('submit', function(e){
-      
       e.preventDefault();
+      // $('#characterModel').css("margin-left", "25px");
     
 
       if(keysToType.length === 0) {
+        $('#characterModel').animate({
+        marginLeft: "+=40"
+          }, 500, function() {
+        //Complete
+          }
+        );
         $userInput.val('');
         level++;
-        $currentScore++;
-        $('#score').append($currentScore);
+        score++;
+        $currentScore.prepend(score);
         keysToType = window.sentences[level].split("");
         $typeThis.text(keysToType.join(""));
+        console.log("hurrah");
       }
-    });
+    })
   };
-  
-
-
-  
-
-
 });
+  
+  
+
 
 
 
