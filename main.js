a7d6c593451c91c586c9c61035d3ece748220d56
Jeremy Smith <jmsmith57@hotmail.com>
2016-07-09 15:55:40 +0100

    player model and submit is working and code is tidier

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-// console.log ("js is working")
-//   console.log(sentences);
-// take the sentences and put them in the span id="typeThis"
-//make the span appear on the screen 
-//get text input to work and store that in an array so that it can be compared to the original sentence.
-//get it to at least work once
 $(function(){
   var level = 0;
   var keysToType = window.sentences[level].split("");
@@ -11,45 +5,54 @@ $(function(){
   var $userInput = $("#userInput");
   var $typeThis = $("#typeThis");
   var $gameStart = $("#gameStart");
-  var $score = $("#score")
-  // var clearOnSubmit = false;
+  var $characterModel = $("#characterModel");
+  var $currentScore = $("#score");
 
+  $characterModel.hide();
   $gameStart.on('click', function(){
-    console.log(keysToType);
     $(this).fadeOut('slow');
-    $score.show();
     $userInput.focus();
-    initEventHandlers();
-    //  //right a transition to the game starting so it should go down into the game starting below 
+    setTimeout(function(){
+      initEventHandlers();
+      $characterModel.show();
+    },2000);
   });
 
   
   function initEventHandlers(){
     $typeThis.prepend(keysToType);
     $userInput.keydown(function() {
-         console.log(event.key);
       
       if(event.key === keysToType[0]) {
         keysToType.shift();
-        $typeThis.text(keysToType.join(""));//write some kind of transition for the letters maybe add something extra to show whether you are right or wrong as you type
-        console.log(keysToType);
-      }
+        $typeThis.text(keysToType.join(""));
+        // setTimeout(function(){
+        //   $typeThis.append("PRESS ENTER");
+        // }, 3000);
+      }else if (event.key!== keysToType[0]){
+        
+      };
     });
 
     $userInputForm.on('submit', function(e){
       
       e.preventDefault();
+    
 
       if(keysToType.length === 0) {
-//figure out a way to show that you won or loss maybe a pass score to continue to the next level and put in an array within an array  
         $userInput.val('');
         level++;
+        $currentScore++;
+        $('#score').append($currentScore);
         keysToType = window.sentences[level].split("");
-        $typeThis.text(keysToType.join(""));//this is the next sentence 
-        console.log("Hurrah!");
+        $typeThis.text(keysToType.join(""));
       }
     });
   };
+  
+
+
+  
 
 
 });
